f895ac39057999331fcd4baed03595b780962622
Gabriel <gmaestre02@hotmail.com>
2020-07-31 11:36:52 -0300

    Tool to "Broken Windows". Restart the (explorer.exe).

---
+++ RestartExplorer.exe/RestartExplorer.exe/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace RestartExplorer.exe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKill = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.chkStartup = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnKill
+            // 
+            this.btnKill.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKill.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKill.ForeColor = System.Drawing.Color.White;
+            this.btnKill.Location = new System.Drawing.Point(6, 4);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(125, 36);
+            this.btnKill.TabIndex = 0;
+            this.btnKill.Text = "Kill Explorer.exe";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Location = new System.Drawing.Point(6, 46);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(125, 36);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start Explorer.exe";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.ForeColor = System.Drawing.Color.White;
+            this.btnRestart.Location = new System.Drawing.Point(141, 12);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(125, 57);
+            this.btnRestart.TabIndex = 2;
+            this.btnRestart.Text = "Restart Explorer.exe";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // chkStartup
+            // 
+            this.chkStartup.AutoSize = true;
+            this.chkStartup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkStartup.ForeColor = System.Drawing.Color.White;
+            this.chkStartup.Location = new System.Drawing.Point(66, 86);
+            this.chkStartup.Name = "chkStartup";
+            this.chkStartup.Size = new System.Drawing.Size(157, 19);
+            this.chkStartup.TabIndex = 3;
+            this.chkStartup.Text = "Run at Windows Startup";
+            this.chkStartup.UseVisualStyleBackColor = true;
+            this.chkStartup.CheckedChanged += new System.EventHandler(this.chkStartup_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(278, 111);
+            this.Controls.Add(this.chkStartup);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnKill);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Restart explorer.exe - Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKill;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.CheckBox chkStartup;
+    }
+}
+
---
+++ RestartExplorer.exe/RestartExplorer.exe/Form1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RestartExplorer.exe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //BY gmaestre002@gmail.com
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+            Process processo = new Process();
+
+            processo.StartInfo = new ProcessStartInfo("taskkill", "/F /IM explorer.exe");
+
+            processo.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            processo.Start();
+
+            processo.WaitForExit();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Process.Start(Path.Combine(Environment.GetEnvironmentVariable("windir"), "explorer.exe"));
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process processo = new Process();
+
+                processo.StartInfo = new ProcessStartInfo("taskkill", "/F /IM explorer.exe");
+
+                processo.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                processo.Start();
+
+                processo.WaitForExit();
+
+                Process.Start(Path.Combine(Environment.GetEnvironmentVariable("windir"), "explorer.exe"));
+
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.ToString() + "\n Contact the Progammer", "ERROR");
+            }
+        }
+
+        private void chkStartup_CheckedChanged(object sender, EventArgs e)
+        {
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey
+            ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (chkStartup.Checked)
+            {
+                registryKey.SetValue("RestartExplorer.exe", Application.ExecutablePath);
+            }
+            else
+            {
+                registryKey.DeleteValue("RestartExplorer.exe");
+            }
+        }
+    }
+}
